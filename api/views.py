1d0af6583324abc82bb35beae916f0538292a5f6
eslleyrubem <rubemeslley@gmail.com>
2023-05-27 17:30:52 -0300

    improvement: validates request

--- api/views.py
+++ api/views.py
@@ -3,11 +3,14 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 
 from api.antlr.main import *
+from api.serializers import InputSerializer
 
 @api_view(['POST'])
 def analisys(request):
-    data = request.data['input']
-    
-    tokens = processInput(data)
-    
-    return Response({ 'tokens': tokens }, status=status.HTTP_200_OK)
+    serializer = InputSerializer(data=request.data)
+    if serializer.is_valid():
+        data = serializer.validated_data
+        tokens = processInput(data['input'])
+        return Response({'tokens': tokens}, status=status.HTTP_200_OK)
+    else:
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
